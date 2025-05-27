d09f99bb457d4f4c8b23229fb97a72264605f3b2
Dawid Staśkiewicz <d.staskiewicz@b2b.one>
2025-05-27 17:36:35 +0200

    add custom component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import CustomComponent from '@/components/CustomComponent.vue'
 </script>
 
 <template>
-  <div>Calendar component</div>
+  <main>
+    <h1>Calendar component</h1>
+    <CustomComponent />
+  </main>
 </template>
 
 <style scoped>
