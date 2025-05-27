91396c36934ae3208f7b2cb8dba493980c57e33e
Dawid Staśkiewicz <d.staskiewicz@b2b.one>
2025-05-27 18:30:47 +0200

    wip date inputs

--- src/components/CustomComponent.vue
+++ src/components/CustomComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {computed, ref} from "vue";
+import { computed, ref } from "vue";
 
 defineProps({
   selectOptions: Array,
@@ -9,7 +9,12 @@ const selectedTimeUnit = ref()
 const inputValue = ref()
 
 const timeUnits = ['year', 'quarter', 'month', 'week', 'day', 'hour', 'minute']
+const datesFrom = ['date from-to', 'date-from']
+const datesTo = ['date from-to', 'date-to']
+
 const isTimeUnit = computed(() => timeUnits.includes(selectedTimeUnit.value))
+const isDateFrom = computed(() => datesFrom.includes(selectedTimeUnit.value))
+const isDateTo = computed(() => datesTo.includes(selectedTimeUnit.value))
 </script>
 
 <template>
@@ -17,11 +22,11 @@ const isTimeUnit = computed(() => timeUnits.includes(selectedTimeUnit.value))
     <select v-model="selectedTimeUnit">
       <option disabled value="">Please select one</option>
       <option
-        v-for="time in timeUnits"
-        :key="time"
-        :value="time"
+        v-for="item in selectOptions"
+        :key="item"
+        :value="item"
       >
-        {{ time }}
+        {{ item }}
       </option>
     </select>
 
@@ -33,6 +38,9 @@ const isTimeUnit = computed(() => timeUnits.includes(selectedTimeUnit.value))
       />
       {{ selectedTimeUnit }}(s)
     </div>
+
+
+    DF: {{isDateFrom}} - DT: {{isDateTo}}
   </div>
 </template>
 
