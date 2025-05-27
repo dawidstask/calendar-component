f26cfe8233a3343af598295ba8c6e3fad97e96f0
Dawid Staśkiewicz <d.staskiewicz@b2b.one>
2025-05-27 17:43:12 +0200

    add select

--- src/components/CustomComponent.vue
+++ src/components/CustomComponent.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import {ref} from "vue";
 
+const timeUnit = ref()
 </script>
 
 <template>
   <div>
-    custom component
+    <div>Selected: {{ timeUnit }}</div>
+    <select v-model="timeUnit">
+      <option disabled value="">Please select one</option>
+      <option>year</option>
+      <option>quarter</option>
+      <option>month</option>
+    </select>
   </div>
 </template>
 
